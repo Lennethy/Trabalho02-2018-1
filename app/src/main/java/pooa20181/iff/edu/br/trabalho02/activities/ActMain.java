71c883f66472455ded412ca064b5c73b797f84d3
Rodrigo Nunes <rodrigonunes.k2@gmail.com>
2018-07-01 19:00:52 -0300

    Organização de Pacotes

--- app/src/main/java/pooa20181/iff/edu/br/trabalho02/activities/ActMain.java
+++ app/src/main/java/pooa20181/iff/edu/br/trabalho02/activities/ActMain.java
@@ -1,8 +1,10 @@
 
-package pooa20181.iff.edu.br.trabalho02;
+package pooa20181.iff.edu.br.trabalho02.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import pooa20181.iff.edu.br.trabalho02.R;
 
 public class ActMain extends AppCompatActivity {
 
