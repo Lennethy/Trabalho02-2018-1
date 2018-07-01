6fbf957d25821a9d30231fcf1b4e43ed26d7f7eb
Rodrigo Nunes <rodrigonunes.k2@gmail.com>
2018-07-01 20:44:58 -0300

    Ajustes Main Activity

--- app/src/main/java/pooa20181/iff/edu/br/trabalho02/activities/ActMain.java
+++ app/src/main/java/pooa20181/iff/edu/br/trabalho02/activities/ActMain.java
@@ -1,16 +1,48 @@
 
 package pooa20181.iff.edu.br.trabalho02.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import pooa20181.iff.edu.br.trabalho02.R;
 
-public class ActMain extends AppCompatActivity {
+public class ActMain extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private String[] activities = {"Candidatos", "Eleitores"};
+    private String[] itemMenu = {"Candidato", "Eleitor"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemMenu);
+
+        ListView listView = (ListView) findViewById(R.id.listaMenu);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
+        Intent intent = null;
+
+        try {
+            Class obj = Class.forName("pooa20181.iff.edu.br.trabalho02.activity." + activities[position]);
+            intent = new Intent(this, obj);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        startActivity(intent);
     }
 }
+
+
+
