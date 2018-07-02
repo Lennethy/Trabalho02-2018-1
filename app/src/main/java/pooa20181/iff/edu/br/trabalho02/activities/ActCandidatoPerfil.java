e98fec78e36c4d35b9d2293648c82acfa6ad2a23
Rodrigo Nunes <rodrigonunes.k2@gmail.com>
2018-07-02 01:25:06 -0300

    Activity Perfil Candidato

---
+++ app/src/main/java/pooa20181/iff/edu/br/trabalho02/activities/ActCandidatoPerfil.java
@@ -0,0 +1,146 @@
+package pooa20181.iff.edu.br.trabalho02.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import  pooa20181.iff.edu.br.trabalho02.R;
+import  pooa20181.iff.edu.br.trabalho02.models.Candidato;
+import io.realm.Realm;
+
+public class ActCandidatoPerfil extends AppCompatActivity {
+
+    EditText edtNome, edtPartido, edtCargo, edtNumeroVotos, edtNumeroUrna, edtEstado, edtMunicipio;
+    Button btnAdicionar, btnAlterar, btnExcluir;
+
+
+    Candidato candidato;
+    int id;
+    private Realm realm;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_candidato_perfil);
+
+        edtNome = (EditText) findViewById(R.id.edtNomeEleitor);
+        edtPartido = (EditText)findViewById(R.id.edtNomeMae);
+        edtCargo = (EditText) findViewById(R.id.edtNumeroTitulo);
+        edtNumeroVotos = (EditText) findViewById(R.id.edtZona);
+        edtNumeroUrna = (EditText) findViewById(R.id.edtDataNascimento);
+        edtEstado = (EditText) findViewById(R.id.edtSecao);
+        edtMunicipio = (EditText) findViewById(R.id.edtMunicipio);
+        btnAdicionar = (Button) findViewById(R.id.btnAdicionar);
+        btnAlterar = (Button) findViewById(R.id.btnAltera);
+        btnExcluir = (Button) findViewById(R.id.btnExclui);
+
+        Intent intent = getIntent();
+        id = (int) intent.getSerializableExtra("id");
+        realm = Realm.getDefaultInstance();
+
+        if(id != 0) {
+
+            btnAdicionar.setEnabled(false);
+            btnAdicionar.setClickable(false);
+            btnAdicionar.setVisibility(View.INVISIBLE);
+            candidato = realm.where(Candidato.class).equalTo("id", id).findFirst();
+            edtNome.setText(candidato.getNome());
+            edtPartido.setText(candidato.getPartido());
+            edtNumeroVotos.setText(candidato.getNumVotos());
+            edtCargo.setText(candidato.getCargo());
+            edtNumeroUrna.setText(candidato.getNumUrna());
+            edtEstado.setText(candidato.getEstado());
+            edtMunicipio.setText(candidato.getMunicipio());
+        }
+        else{
+            btnAlterar.setEnabled(false);
+            btnAlterar.setClickable(false);
+            btnAlterar.setVisibility(View.INVISIBLE);
+            btnExcluir.setEnabled(false);
+            btnExcluir.setClickable(false);
+            btnExcluir.setVisibility(View.INVISIBLE);
+        }
+
+        btnAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                salvar();
+            }
+        });
+
+        btnExcluir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                excluir();
+            }
+        });
+
+        btnAlterar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alterar();
+            }
+        });
+
+    }
+    public void excluir()
+    {
+        realm.beginTransaction();
+        candidato.deleteFromRealm();
+        realm.commitTransaction();
+        realm.close();
+        Toast.makeText(this, "Candidato Excluido", Toast.LENGTH_LONG).show();
+        this.finish();
+
+    }
+
+    public void salvar()
+    {
+        int proximoID = 1;
+        if(realm.where(Candidato.class).max("id") != null)
+        {
+            proximoID = realm.where(Candidato.class).max("id").intValue()+1;
+        }
+
+        realm.beginTransaction();
+        Candidato candidato = new Candidato();
+        candidato.setId(proximoID);
+        setEgrava(candidato);
+        realm.copyToRealm(candidato);
+        realm.commitTransaction();
+        realm.close();
+        Toast.makeText(this, "Candidato Cadastrado", Toast.LENGTH_LONG).show();
+        this.finish();
+    }
+
+    public void alterar()
+    {
+        realm.beginTransaction();
+        setEgrava(candidato);
+
+        realm.copyFromRealm(candidato);
+        realm.commitTransaction();
+        realm.close();
+
+        Toast.makeText(this, "Candidato Alterado", Toast.LENGTH_LONG).show();
+        this.finish();
+    }
+
+    public void setEgrava(Candidato candidato)
+    {
+        candidato.setNome(edtNome.getText().toString());
+        candidato.setPartido(edtPartido.getText().toString());
+        candidato.setNumVotos(edtNumeroVotos.getText().toString());
+        candidato.setNumUrna(edtNumeroUrna.getText().toString());
+        candidato.setCargo(edtCargo.getText().toString());
+        candidato.setEstado(edtEstado.getText().toString());
+        candidato.setMunicipio(edtMunicipio.getText().toString());
+    }
+
+
+
+
+}
